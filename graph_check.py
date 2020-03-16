501cf84f572a3596844906e663730f2b02fd2fc8
jacquesboitreaud <jacquesboitreaud@users.noreply.github.com>
2020-03-16 14:35:16 -0400

    debug

--- graph_check.py
+++ graph_check.py
@@ -6,11 +6,19 @@ Created on Mon Mar 16 14:30:04 2020
 """
 import pickle
 import networkx as nx
+import sys, os 
 
-with open('data/chunks/5i4a.pickle','rb') as f:
-    g = pickle.load(f)
+if __name__ == "__main__":
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    sys.path.append(script_dir)
+    sys.path.append(os.path.join(script_dir,'data_processing'))
+    from data_processing.rna_classes import *
     
-print(g.nodes(data=True))
+    
+    with open('data/chunks/5i4a.pickle','rb') as f:
+        g = pickle.load(f)
+        
+    print(g.nodes(data=True))
     
     
 
