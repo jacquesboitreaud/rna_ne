c60d253bc49cabcf6d3b2eff5d268dc29837efed
jacquesboitreaud <jacquesboitreaud@users.noreply.github.com>
2020-03-30 11:50:04 +0200

    debug

--- data_processing/dataset_cut.py
+++ data_processing/dataset_cut.py
@@ -5,19 +5,30 @@ Created on Mon Mar 30 10:47:13 2020
 @author: jacqu
 """
 
-with open('selected_nodes.pickle','rb') as f:
+with open('../data/all_nodes.pickle','rb') as f:
     d = pickle.load(f)
     
 keys = list(d.keys())
 
 train, valid = keys[:-10], keys[-10:]
 
-
-
 valid_nodes = {k:d[k] for k in valid  }
+
+
 
 for k in valid : 
     d.pop(k)
+    
+bads=[]
+for k,v in d.items():
+    if(len(v)==0):
+        print('zero!')
+        print(k)
+        bads.append(k)
+        
+for k in bads :
+    d.pop(k)
+        
     
 with open('train_nodes.pickle','wb') as f:
     pickle.dump(d,f)
